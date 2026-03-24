5f7f4dfe24d1dbb1506ceac9589570e7e3a3908b
Lê Trọng Hiếu <168757976+SnuggieBoy@users.noreply.github.com>
2026-03-24 15:43:31 +0700

    update

--- modules/audioGuide.js
+++ modules/audioGuide.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 
 let sound;
 let bufferLoaded = false; // flag to track if audio buffer is loaded
+let shouldPlay = false; // flag for when the user starts the experience before the audio load completes
 
 // setup audio for the scene
 export const setupAudio = (camera) => {
@@ -12,17 +13,21 @@ export const setupAudio = (camera) => {
   sound = new THREE.Audio(listener); // creating the audio source
 
   const audioLoader = new THREE.AudioLoader(); // create an audio loader
-  audioLoader.load("audio/ambient.ogg", function (buffer) {
+  audioLoader.load("sounds/tiersen.mp3", function (buffer) {
     // load the audio file
     sound.setBuffer(buffer); // set the audio source buffer
     sound.setLoop(true); // set the audio source to loop
-    sound.setVolume(0.3); // soothe the volume
-    bufferLoaded = true; // set bufferLoaded flag to true once the audio buffer is loaded
+    sound.setVolume(0.4); // soothing volume
+    bufferLoaded = true; // flag ready
+    if (shouldPlay) {
+      sound.play(); // Auto-start if user already clicked Start
+    }
   });
 };
 
 // play audio
 export const startAudio = () => {
+  shouldPlay = true;
   if (sound && bufferLoaded) {
     // check if the buffer is loaded before playing
     sound.play();
@@ -31,7 +36,8 @@ export const startAudio = () => {
 
 // pause audio
 export const stopAudio = () => {
-  if (sound) {
+  shouldPlay = false;
+  if (sound && sound.isPlaying) {
     sound.pause();
   }
 };
